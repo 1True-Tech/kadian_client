e196690a1ba66e2a754164601c5642b1799efa7c
p34-pac <peterpaultolulope@gmail.com>
2025-07-30 15:41:37 +0100

    update carousel
    fixed carousel layout shift in hero section

--- components/pages/home/HeroSection/heroItem.tsx
+++ components/pages/home/HeroSection/heroItem.tsx
@@ -35,7 +35,7 @@ export default function HeroItem({ image }: Props) {
   // console.log(i)
   return (
     <CarouselItem
-      className="max-w-[95%] lg:w-md md:max-w-[50%] !pl-5 md:!pl-7.5 h-full relative overflow-visible isolate"
+      className="max-w-[90%] lg:w-md md:max-w-[calc(50%)] not-first:!pl-5 md:!pl-6 h-full relative overflow-visible isolate"
       style={{
         color: generateAccessibleColorPair({
           primary: imageColors.bgColor,
