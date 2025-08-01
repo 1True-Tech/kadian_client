d69d37257b5b78a6d437c60d568cc5280046a0cb
p34-pac <peterpaultolulope@gmail.com>
2025-08-01 16:28:55 +0100

    feat: update StudioPage metadata and viewport settings; comment out blogPost schema

--- app/(ADMIN)/admin/cms-studio/[[...tool]]/page.tsx
+++ app/(ADMIN)/admin/cms-studio/[[...tool]]/page.tsx
@@ -1,9 +1,20 @@
 import RenderStudio from "@/cms-studio/lib/renderStudio";
 import config from "@/sanity.config";
+import { Metadata, Viewport } from "next";
+import { metadata as studioMetaData, viewport as StudioViewport } from "next-sanity/studio";
+
 
 export const dynamic = "force-static";
+export const metadata:Metadata = {
+  ...studioMetaData,
+  title: "Loading Kadian Studio...",
+  
+};
+export const viewport:Viewport = {
+  ...StudioViewport,
+  interactiveWidget: 'resizes-content'
+};
 
-export { metadata, viewport } from "next-sanity/studio";
 export default function StudioPage() {
   return <RenderStudio config={config}/>;
 }
